13dc78ca1b8232374ffd05caf005ee1b862c342b
Takayoshi Uchida <taum0715@gmail.com>
2015-07-12 15:23:04 +0900

    デザイン微修正

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 $(function(){
 	var $iframe = $('iframe#modalContainer');
-	$iframe.attr({'src':'template/modal.html','style':' height:'+(screen.height-200)+'px;'});
+	$iframe.attr({'src':'template/modal.html','style':'height:'+(screen.height-300)+'px;'});
 });
